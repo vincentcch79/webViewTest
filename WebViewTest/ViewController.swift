cd52c154a0bf48db989d36b3b021b593829e0c72
Vincent Chang <vincentcch79@gmail.com>
2016-06-24 12:15:29 +0800

    Initial Commit

---
+++ WebViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by 張智涵 on 2016/6/24.
+//  Copyright © 2016年 張智涵 Vincent Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
